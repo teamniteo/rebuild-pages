7f887a121ebbdd62176d13f4123349ff353e1c30
Bostjan Cigan <bostjan.cigan@klika.si>
2016-01-13 22:22:53 +0100

    Added custom permalinks manual activation to unit testing.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@ require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/rebuild-pages-from-majestic.php';
+	$custom_permalinks_dir = dirname(dirname(dirname(__FILE__))) . '/custom-permalinks/custom-permalinks.php';
+	require $custom_permalinks_dir;
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
